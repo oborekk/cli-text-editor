34336073a0abeb31cacafdbe63a241eea34fb4fb
Oskar Borek <oskar.borek@securitasdirect.es>
2025-06-17 21:43:11 +0200

    +disable rawmode at exit +ICANON

--- kilo.c
+++ kilo.c
@@ -1,12 +1,19 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
 
+struct termios orig_termios;
+
+void disableRawMode() {
+  tccsetatr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
 void enableRawMode() {
-  struct termios raw;
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
 
-  tcgetattr(STDIN_FILENO, &raw);
-
-  raw.c_lflag &= ~(ECHO);
+  struct termios raw = orig_termios;
+  raw.c_lflag &= ~(ECHO | ICANON);
 
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
